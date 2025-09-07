c927438eaf061d13e308b13369299686047022c7
Pasindu Ravishan <pasinduravishan88@gmail.com>
2025-09-07 11:29:21 +0530

    small update..

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, use, useEffect } from 'react';
+import React,{ useState, use, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
